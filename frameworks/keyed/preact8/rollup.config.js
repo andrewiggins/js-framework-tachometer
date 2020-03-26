9b49bccad9fbd5256370826374fa1d31c32ecde3
Andre Wiggins <andrewiggins@live.com>
2020-03-26 14:13:42 -0700

    Add preact8 framework

---
+++ frameworks/keyed/preact8/rollup.config.js
@@ -0,0 +1,27 @@
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const { terser } = require("rollup-plugin-terser");
+const babel = require("rollup-plugin-babel");
+
+const minify = process.env.MINIFY !== "false";
+
+module.exports = {
+	input: "src/index.js",
+	output: {
+		dir: "dist",
+		format: "es",
+		compact: minify,
+		entryFileNames: `[name].js`,
+		chunkFileNames: `[name]-[hash].js`
+	},
+	plugins: [
+		babel({ exclude: /node_modules/ }),
+		// @ts-ignore - nodeResolve doesn't export .default in cjs import
+		nodeResolve({
+			extensions: [".mjs", ".js", ".jsx", ".json", ".node"]
+		}),
+		minify ? terser() : null
+	],
+	watch: {
+		clearScreen: false
+	}
+};
