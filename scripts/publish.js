361cb4c4af2dc517098c62f257275be85025a82e
Andre Wiggins <andrewiggins@live.com>
2020-03-26 13:19:15 -0700

    Remove gh-pages dependency

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,5 +1,4 @@
 const cpy = require("cpy");
-const ghPages = require("gh-pages");
 const { repoRoot } = require("./lib/paths");
 
 /**
@@ -7,11 +6,6 @@ const { repoRoot } = require("./lib/paths");
  * @param {import('./index').CmdLineOptions} options
  */
 async function publish(_, options) {
-	// if (options.clean) {
-	// 	ghPages.clean();
-	// 	return;
-	// }
-
 	const src = [
 		"node_modules/afterframe/dist/*",
 		"node_modules/spectre.css/dist/*",
@@ -23,28 +17,6 @@ async function publish(_, options) {
 	];
 
 	return cpy(src, "dist", { cwd: repoRoot(), parents: true });
-
-	// return ghPublish(repoRoot(), {
-	// 	src,
-	// 	push: !options.debug
-	// });
-}
-
-/**
- *
- * @param {string} dir
- * @param {import('gh-pages').PublishOptions} options
- */
-function ghPublish(dir, options) {
-	return new Promise((resolve, reject) => {
-		ghPages.publish(dir, options, error => {
-			if (error) {
-				reject(error);
-			} else {
-				resolve();
-			}
-		});
-	});
 }
 
 module.exports = {
