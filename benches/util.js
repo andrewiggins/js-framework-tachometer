23140ab506d8707a73b02636f353f66cbb34feed
CI-master Github Action <action@github.com>
2020-08-18 18:52:17 +0000

    Updates from CI-master#55

--- benches/util.js
+++ benches/util.js
@@ -2,6 +2,122 @@ import afterFrame from "../node_modules/afterframe/dist/afterframe.module.js";
 
 export { afterFrame };
 
+/**
+ * @extends {Console}
+ */
+class ConsoleBuffer {
+	constructor() {
+		/** @type {Array<[string, any[]]>} */
+		this.buffer = [];
+
+		for (let methodName of Object.keys(console)) {
+			this[methodName] = this.proxy(methodName);
+		}
+	}
+
+	proxy(methodName) {
+		return (...args) => {
+			this.buffer.push([methodName, args]);
+		};
+	}
+
+	flush() {
+		let method;
+		while ((method = this.buffer.shift())) {
+			let [name, args] = method;
+			console[name](...args);
+		}
+	}
+}
+
+/** @type {ConsoleBuffer} */
+let consoleBuffer;
+if (window.location.search.includes("logdom=1")) {
+	consoleBuffer = new ConsoleBuffer();
+	logCall(Element.prototype, "after");
+	logCall(Element.prototype, "append");
+	logCall(Element.prototype, "before");
+	// logCall(Element.prototype, "innerHTML");
+	logCall(Element.prototype, "insertAdjacentElement");
+	logCall(Element.prototype, "insertAdjacentHTML");
+	logCall(Element.prototype, "insertAdjacentText");
+	// logCall(Element.prototype, "outerHTML");
+	logCall(Element.prototype, "prepend");
+	logCall(Element.prototype, "replaceWith");
+	// logCall(Element.prototype, "textContent");
+
+	logCall(Element.prototype, "appendChild");
+	logCall(Element.prototype, "insertBefore");
+	logCall(Element.prototype, "replaceChild");
+	logCall(Element.prototype, "removeChild");
+	logCall(Element.prototype, "remove");
+}
+
+let scheduled = false;
+function scheduleFlush() {
+	if (!scheduled && consoleBuffer) {
+		afterFrame(() => {
+			console.group("Flush");
+			consoleBuffer.flush();
+			console.groupEnd();
+			scheduled = false;
+		});
+		scheduled = true;
+	}
+}
+
+/**
+ * Serialize an object
+ * @param {Object} obj
+ * @return {string}
+ */
+function serialize(obj) {
+	let s;
+	if (obj instanceof Text) {
+		s = "#text";
+	} else if (obj instanceof Element) {
+		s = `<${obj.localName}>${obj.textContent}`;
+	} else if (obj === document) {
+		s = "document";
+	} else if (typeof obj == "string") {
+		s = obj;
+	} else {
+		s = Object.prototype.toString.call(obj).replace(/(^\[object |\]$)/g, "");
+	}
+
+	return s.substr(0, 50);
+}
+
+/**
+ * Modify obj's original method to log calls and arguments on logger object
+ * @template T
+ * @param {T} obj
+ * @param {keyof T} method
+ */
+export function logCall(obj, method) {
+	let old = obj[method];
+	obj[method] = function(...args) {
+		let c = "";
+		for (let i = 0; i < args.length; i++) {
+			if (c) c += ", ";
+			c += serialize(args[i]);
+		}
+
+		// Normalize removeChild -> remove to keep output clean and readable
+		const operation =
+			method != "removeChild"
+				? `${serialize(this)}.${method}(${c})`
+				: `${serialize(c)}.remove()`;
+
+		if (consoleBuffer) {
+			consoleBuffer.log(operation);
+			scheduleFlush();
+		}
+
+		return old.apply(this, args);
+	};
+}
+
 let promise = null;
 export function afterFrameAsync() {
 	if (promise === null) {
